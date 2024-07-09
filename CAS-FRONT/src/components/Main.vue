4f9b57e453e27592dbb207c60a1949f03e55291b
znkim <znkim@gaia3d.com>
2024-07-09 18:59:08 +0900

    feat : default map functions - osm imagery layer, vworld toggles - cesium world terrain toggle - osm building tilesets

--- CAS-FRONT/src/components/Main.vue
+++ CAS-FRONT/src/components/Main.vue
@@ -3,8 +3,25 @@ import Map from "./Map.vue";
 import {onMounted, ref} from "vue";
 
 const mapComponent = ref();
+
 const viewer = ref();
 const magoInstance = ref();
+
+const options = ref({
+  infoBox: false,
+  navigationHelpButton: false,
+  selectionIndicator: false,
+  homeButton: false,
+  fullscreenButton: false,
+  geocoder: false,
+  baseLayerPicker: false,
+  vworldToken: 'BB89CEE2-0CBC-3378-A40B-468C4897B788',
+});
+
+/* @ts-ignore */
+const Cesium = window.Cesium;
+/* @ts-ignore */
+const Mago3D = window.Mago3D;
 
 const printViewer = () => {
   console.log(viewer.value);
@@ -14,10 +31,67 @@ const printMagoInstance = () => {
   console.log(magoInstance.value);
 }
 
-onMounted(() => {
+const flyTo = (lon: number, lat: number, height: number, duration: number = 2) => {
+  viewer.value.camera.flyTo({
+    destination: Cesium.Cartesian3.fromDegrees(lon, lat, height),
+    duration: duration
+  });
+}
+
+const toggleOsmLayer = () => {
+  mapComponent.value.changeGlobeColor('#edebe5')
+  mapComponent.value.removeAllImageryLayers();
+  mapComponent.value.changeImageryLayer('https://a.tile.openstreetmap.org');
+}
+const toggleVWorldLayer = () => {
+  mapComponent.value.changeGlobeColor('#686b61');
+  mapComponent.value.removeAllImageryLayers();
+  mapComponent.value.changeVWorldImageryLayer('Satellite', 'jpeg');
+}
+const toggleWorldTerrain = async () => {
+  if (!(viewer.value.terrainProvider instanceof Cesium.CesiumTerrainProvider)) {
+    viewer.value.terrainProvider = await mapComponent.value.changeWorldTerrain();
+  } else {
+    viewer.value.terrainProvider = new Cesium.EllipsoidTerrainProvider();
+  }
+}
+const toggleOsmBuildings = async () => {
+  const osmBuildingsTileset = await mapComponent.value.getOmsBuildingsTileset();
+
+  const duplicatedTileset = viewer.value.scene.primitives._primitives.find((e) => {
+    return e._url === osmBuildingsTileset._url
+  })
+  if (duplicatedTileset !== undefined) {
+    duplicatedTileset.show = !duplicatedTileset.show;
+  } else {
+    viewer.value.scene.primitives.add(osmBuildingsTileset);
+  }
+}
+
+onMounted(async () => {
   console.log('[MainComponent] Mounted Main Component');
   viewer.value = mapComponent.value.getViewer();
   magoInstance.value = mapComponent.value.getMagoInstance();
+
+  //mapComponent.value.changeGlobeColor('#686b61');
+  //mapComponent.value.changeImageryLayer('https://a.tile.openstreetmap.org');
+  // mapComponent.value.removeAllImageryLayers();
+  // mapComponent.value.changeVWorldImageryLayer('Satellite', 'jpeg');
+
+
+  toggleOsmLayer();
+  toggleVWorldLayer();
+
+  flyTo(126.978388, 37.566610, 50000, 0);
+
+  await toggleOsmBuildings();
+  await toggleWorldTerrain();
+
+  /*const osmBuildingsTileset = await mapComponent.value.getOmsBuildingsTileset();
+  viewer.value.scene.primitives.add(osmBuildingsTileset);
+
+  const cesiumWorldTerrain = await mapComponent.value.changeWorldTerrain();
+  viewer.value.terrainProvider = cesiumWorldTerrain;*/
 });
 
 </script>
@@ -26,8 +100,12 @@ onMounted(() => {
   <div class="button-group">
     <button @click="printViewer">Print Viewer</button>
     <button @click="printMagoInstance">Print MagoInstance</button>
+    <button @click="toggleOsmLayer">Toggle OSM Layer</button>
+    <button @click="toggleVWorldLayer">Toggle VWorld Layer</button>
+    <button @click="toggleWorldTerrain">Toggle World Terrain</button>
+    <button @click="toggleOsmBuildings">Toggle OSM Buildings</button>
   </div>
-  <Map ref="mapComponent"/>
+  <Map :init-options="options" ref="mapComponent"/>
 </template>
 
 <style scoped>
@@ -36,7 +114,8 @@ onMounted(() => {
   position: absolute;
   top: 10px;
   left: 10px;
-  z-index: 1000;
+  z-index: 10;
+  opacity: 0.9;
 }
 .button-group button {
   display: block;
--- CAS-FRONT/src/components/Map.vue
+++ CAS-FRONT/src/components/Map.vue
@@ -2,36 +2,110 @@ import "externals/cesium/Widget/Widget.css";
 
 <script setup lang="ts">
   import {onMounted, ref} from "vue";
-  const magoInstance = ref<any>();
-  const viewer = ref<any>();
 
-  /*defineProps<{
-    msg: string
-  }>()*/
+  const props = defineProps<{
+    initOptions: any;
+  }>();
+
+  /* @ts-ignore */
+  const Cesium = window.Cesium;
+  const magoInstance = ref<any>();
+  const cesiumViewer = ref<any>();
+
+  const omsBuildingsTileset = ref<any>();
+  const cesiumWorldTerrain = ref<any>();
 
   onMounted(() => {
     console.log('[MapComponent] Mounted');
-    const options : any = {};
-    options.infoBox = false;
-    options.navigationHelpButton = false;
-    options.selectionIndicator = false;
-    options.homeButton = false;
-    options.fullscreenButton = false;
-    options.geocoder = false;
-    options.baseLayerPicker = false;
-
     /* @ts-ignore */
-
-    const newMagoInstance = new Mago3D.Mago3d("mago3dContainer", {}, {loadend: () => {}}, options);
+    const newMagoInstance = new Mago3D.Mago3d("mago3dContainer", {}, {loadend: () => {}}, props.initOptions);
     magoInstance.value = newMagoInstance;
-    viewer.value = newMagoInstance.getViewer()
+    cesiumViewer.value = newMagoInstance.getViewer()
+
+    Cesium.Ion.defaultAccessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI5OTEzYmExNS1lNjI1LTQxNWUtOGFkNS0zM2U0Y2FkMjA4OGMiLCJpZCI6MTY3ODk5LCJpYXQiOjE2OTUzNTYxMTl9.7Alxx-4hzrnSBkbz1DvClO8eQX38sBVzTKXYI-lI8_g"
 
     console.log('[MapComponent] mago3dInstance', magoInstance);
-    console.log('[MapComponent] viewer', viewer);
+    console.log('[MapComponent] viewer', cesiumViewer);
   });
 
+  const changeImageryLayer = (url: string) => {
+    const viewer = cesiumViewer.value
+    const scene = viewer.scene
+    const layers = scene.imageryLayers
+    layers.removeAll()
+
+    /* @ts-ignore */
+    const osm = new Cesium.OpenStreetMapImageryProvider({url: url})
+    layers.addImageryProvider(osm)
+    return osm
+  }
+
+  const changeGlobeColor = (cssColor: string = '#000000') => {
+    const viewer = cesiumViewer.value
+    const scene = viewer.scene
+    scene.globe.baseColor = Cesium.Color.fromCssColorString(cssColor)
+  }
+
+  const removeAllImageryLayers = () => {
+    const viewer = cesiumViewer.value
+    const scene = viewer.scene
+    const layers = scene.imageryLayers
+    layers.removeAll()
+  }
+
+  const changeVWorldImageryLayer = (type: string, extension: string) => {
+    const viewer = cesiumViewer.value
+    const scene = viewer.scene
+    const layers = scene.imageryLayers
+
+    const VWORD_KEY = props.initOptions.vworldToken
+    const minLevel = 5
+    const maxLevel = 19
+
+    const protocol = location.protocol === 'https:' ? 'https' : 'http'
+    const options = {
+      url: `${protocol}://api.vworld.kr/req/wmts/1.0.0/${VWORD_KEY}/${type}/{TileMatrix}/{TileRow}/{TileCol}.${extension}`,
+      layer: 'Base',
+      style: 'default',
+      maximumLevel: maxLevel,
+      tileMatrixSetID: 'default028mm'
+    }
+    const imageryProvider = new Cesium.WebMapTileServiceImageryProvider(options)
+    const imageryLayer = new Cesium.ImageryLayer(imageryProvider, {
+      show: true,
+      minimumTerrainLevel: minLevel
+    });
+    layers.add(imageryLayer)
+  }
+
+  const changeWorldTerrain = async () => {
+    if (cesiumWorldTerrain.value) {
+      return cesiumWorldTerrain.value;
+    }
+    const worldTerrain = await Cesium.createWorldTerrainAsync();
+    cesiumWorldTerrain.value = worldTerrain;
+    return worldTerrain;
+  }
+
+  const getOmsBuildingsTileset = async () => {
+    if (omsBuildingsTileset.value) {
+      return omsBuildingsTileset.value;
+    }
+
+    const osmBuildingsTileset = await Cesium.createOsmBuildingsAsync({
+      enableShowOutline: false,
+      showOutline: false,
+    });
+    osmBuildingsTileset.colorBlendMode = Cesium.Cesium3DTileColorBlendMode.REPLACE;
+    osmBuildingsTileset.style = new Cesium.Cesium3DTileStyle({
+      color: "color('#ffffff')",
+    });
+    omsBuildingsTileset.value = osmBuildingsTileset;
+    return osmBuildingsTileset;
+  }
+
   const getViewer = () => {
-    return viewer.value;
+    return cesiumViewer.value;
   }
 
   const getMagoInstance = () => {
@@ -40,7 +114,13 @@ import "externals/cesium/Widget/Widget.css";
 
   defineExpose({
     getViewer,
-    getMagoInstance
+    getMagoInstance,
+    changeGlobeColor,
+    changeImageryLayer,
+    changeVWorldImageryLayer,
+    changeWorldTerrain,
+    getOmsBuildingsTileset,
+    removeAllImageryLayers
   });
 </script>
 
